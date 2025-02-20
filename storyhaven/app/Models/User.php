79af91074b19a0c0767a2201a8c9a2f38967bead
Francisco Manuel Hernández <francisco-h-j@hotmail.com>
2025-02-20 13:00:59 +0100

    feat(storyhaven): files added

--- storyhaven/app/Models/User.php
+++ storyhaven/app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function relatos()
+    {
+        return $this->hasMany(Relato::class);
+    }
 }
